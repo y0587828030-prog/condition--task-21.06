993479143c20848a7c4d801a94ab4f69056ca046
yehoshua <y0587828030@gmail.com>
2026-06-21 21:45:31 +0300

    Cave options

--- minu apps.py
+++ minu apps.py
@@ -5,6 +5,18 @@ match trip:
         if choose == "hide" :
             print("you hide behind a tree ")
         elif choose == "walk":
-            print("you find a sleeping wolf")
+            print("you find a sleeping wolf ")
         else:
-            print("unvalid forest action")
+            print("unvalid forest action ")
+    case "cave":
+        choose=input("do you have a torch ")
+        if choose == "yes":
+            turn=input("left or right ")
+            if turn == "left":
+                print("you find gold ")
+            elif turn == "right":
+                print("you find bats")
+            else:
+                print("invalid cave path ")
+        else:
+            print("its too dark to enter")
