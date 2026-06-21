e4900e37473041141cc6573d5572291f2ae04d9b
yehoshua <y0587828030@gmail.com>
2026-06-21 15:06:51 +0300

    task_4

--- conditions.py
+++ conditions.py
@@ -5,19 +5,27 @@
 # else:
 #     print("cannot enter")
 
-#stap 2
-temperature = 38.2
-if temperature > 37.5:
-    print("High temperature")
-else:
-    print("High temperature")
+# #stap 2
+# temperature = 38.2
+# if temperature > 37.5:
+#     print("High temperature")
+# else:
+#     print("High temperature")
 
-#stap 3
-geiven_namber = int(input("geiven namber "))
-if geiven_namber % 2 == 0:
-    print("even number")
-else:
-    print("odd number")
+# #stap 3
+# geiven_namber = int(input("geiven namber "))
+# if geiven_namber % 2 == 0:
+#     print("even number")
+# else:
+#     print("odd number")
+
+#stap 4
+define_battery = 15
+is_charging = True
+if define_battery < 20 and is_charging == True:
+    print("low battery, charging now")
+
+
 
 
 
