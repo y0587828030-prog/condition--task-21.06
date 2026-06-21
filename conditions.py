e605b0887a3d36150d3f57b700dc620da58a5741
yehoshua <y0587828030@gmail.com>
2026-06-21 14:49:10 +0300

    task 1

---
+++ conditions.py
@@ -0,0 +1,28 @@
+#stap 1
+# geiven_age = int(input("what is your age? "))
+# if geiven_age > 17:
+#     print("can enter")
+# else:
+#     print("cannot enter")
+
+#stap 2
+temperature = 38.2
+if temperature > 37.5:
+    print("High temperature")
+else:
+    print("High temperature")
+
+
+
+
+
+
+
+
+# name_1 = "yehosh"
+# name_2 = "josh"
+
+# match_name: 
+# case"yehosh"
+#         print("admin")
+
